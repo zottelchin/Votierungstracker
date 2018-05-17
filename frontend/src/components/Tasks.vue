964a8940d02f0aed94d141d3295de8bc73fdd539
Unknown <zottelchin@gmail.com>
2018-05-17 21:13:14 +0200

    Adding Settingsmenu for course

--- frontend/src/components/Tasks.vue
+++ frontend/src/components/Tasks.vue
@@ -5,8 +5,38 @@
         <span><!--aktueller Kurs:--> <b>{{ course }}</b></span>
       </div>
       <router-link :to="'/'+encodeURIComponent(account)"><i class="fa fa-chevron-left"></i> Zurück zu meinen Kursen</router-link>
-      <i class="fas fa-cog right"></i>
-      
+      <i class="fas fa-cog right" @click="settings = !settings"></i>
+      <transition name="grow">
+        <div v-show="settings" class="container settings">
+          <div class="row">
+            <h3>Einstellungen</h3>
+          </div>
+          <div class="row">
+            <span>Prozent </span>  
+              <label class="switch">
+                  <input type="checkbox" v-model="showPoints">
+                  <span class="slider round"></span>
+                </label>
+                <span>Punkte</span>
+          </div>
+          <div class="row">
+              <div v-show="!showPoints">
+              <label for="Prozent" style="font-weight: normal; display: inline;">mindestens zu erreichende Prozent</label>
+              <input id="Prozent" type="number" style="max-width: 65px; margin-left: 2px; display: inline;" min="0" max="100"  v-model="percent">
+              </div>
+              <div v-show="showPoints">
+                <label for="zuErreichen" style="font-weight: normal; display: inline;">mindestens zu erreichende Punkte</label>
+              <input id="zuErreichen" type="number" style="max-width: 65px; margin-left: 2px; display: inline;" min="0" max="100" v-model="zuErreichen">
+              <label for="von" style="font-weight: normal; display: inline;">von</label>
+              <input id="von" type="number" style="max-width: 65px; margin-left: 2px; display: inline;" min="0" max="100" v-model="von">
+              
+              </div>
+              </div>
+            <div class="row">
+              <button class="button" @click="changeP">Speichern</button>
+          </div>
+        </div>
+      </transition>
     </div>
     <table class="tasks">
       <thead>
@@ -44,7 +74,7 @@
             <b>{{ gesPres }}</b>
           </td>
           <td>
-            {{ gesPerc }}
+            <b style="margin-right:3px;">{{ gesPerc }}</b><span>/</span><b style="opacity:0.7; margin-left:3px;">{{ perc }}</b>
           </td>
         </tr>
       </tbody>
@@ -76,84 +106,135 @@
 </template>
 
 <script>
-  import api from "../api"
-  export default {
-    data() {
-      return {
-          gridColumns: ['Aufgabe', 'Punkte', 'Vorträge'],
-          gridColumnsShort: ['name', 'points', 'maxPoints', "presentations"],
-          gridData: [],
-          editData: { maxPoints: 0, points: 0, name: "", presentations: 0, id: 0 },
-          isNew: true,
-          account: this.$route.params.account,
-          course: this.$route.params.course,
+import api from "../api";
+export default {
+  data() {
+    return {
+      gridColumns: ["Aufgabe", "Punkte", "Vorträge"],
+      gridColumnsShort: ["name", "points", "maxPoints", "presentations"],
+      gridData: [],
+      editData: { maxPoints: 0, points: 0, name: "", presentations: 0, id: 0 },
+      isNew: true,
+      account: this.$route.params.account,
+      course: this.$route.params.course,
+      settings: false,
+      percent: 70,
+      perc: "70%",
+      showPoints: false,
+      von: 100,
+      zuErreichen: 70,
+    };
+  },
+  created() {
+    api
+      .GET(
+        "/tasks/" +
+          encodeURIComponent(this.account) +
+          "/" +
+          encodeURIComponent(this.course)
+      )
+      .then(res => {
+        this.gridData = res.body || [];
+      });
+  },
+  methods: {
+    addTask: async function() {
+      var res = await api.POST(
+        "/tasks/" +
+          encodeURIComponent(this.account) +
+          "/" +
+          encodeURIComponent(this.course),
+        this.editData
+      );
+      this.gridData.push({ ...this.editData, id: res.body.created });
+      this.editData = { maxPoints: 0, points: 0, name: "", presentations: 0 };
+    },
+    enter: function() {
+      if (this.isNew) {
+        this.addTask();
+      } else {
+        this.saveEditTask();
       }
     },
-    created() {
-      api.GET('/tasks/' + encodeURIComponent(this.account) + '/' + encodeURIComponent(this.course)).then(res => {
-        this.gridData = res.body || []
-      })
+    setFocus: function(id) {
+      document.getElementById(id).focus();
     },
-    methods: {
-      addTask: async function () {
-        var res = await api.POST('/tasks/' + encodeURIComponent(this.account) + '/' + encodeURIComponent(this.course), this.editData)
-        this.gridData.push({ ...this.editData, id: res.body.created })
-        this.editData = { maxPoints: 0, points: 0, name: "", presentations: 0 }
-      },
-      enter: function(){
-        if (this.isNew){
-          this.addTask()
-        }else{
-          this.saveEditTask()
-        }
-      },
-      setFocus: function(id){
-        document.getElementById(id).focus()
-      },
-      deleteItem: function(delId, id) {
-        this.gridData.splice(delId, 1)
-        api.DELETE("/tasks/" + encodeURIComponent(this.account) + '/' + encodeURIComponent(this.course) + '/' + encodeURIComponent(id))
-      },
-      edit: function(row){
-        this.isNew = false
-        for (let i in this.editData) this.editData[i] = row[i];
-      },
-      saveEditTask: async function() {
-        //Upload changes
-        var res = await api.PUT('/tasks/' + encodeURIComponent(this.account) + '/' + encodeURIComponent(this.course) + '/' + encodeURIComponent(this.editData.id), this.editData)
-        this.gridData.forEach((v, i, a) => { if (v.id == this.editData.id) a.splice(i, 1, this.editData); })
+    deleteItem: function(delId, id) {
+      this.gridData.splice(delId, 1);
+      api.DELETE(
+        "/tasks/" +
+          encodeURIComponent(this.account) +
+          "/" +
+          encodeURIComponent(this.course) +
+          "/" +
+          encodeURIComponent(id)
+      );
+    },
+    edit: function(row) {
+      this.isNew = false;
+      for (let i in this.editData) this.editData[i] = row[i];
+    },
+    saveEditTask: async function() {
+      //Upload changes
+      var res = await api.PUT(
+        "/tasks/" +
+          encodeURIComponent(this.account) +
+          "/" +
+          encodeURIComponent(this.course) +
+          "/" +
+          encodeURIComponent(this.editData.id),
+        this.editData
+      );
+      this.gridData.forEach((v, i, a) => {
+        if (v.id == this.editData.id) a.splice(i, 1, this.editData);
+      });
 
-        this.isNew = true
-        this.editData = { maxPoints: 0, points: 0, name: "", presentations: 0, id: 0 }
+      this.isNew = true;
+      this.editData = {
+        maxPoints: 0,
+        points: 0,
+        name: "",
+        presentations: 0,
+        id: 0
+      };
+    },
+    changeP: function() {
+      if (this.showPoints){
+        this.perc = Math.round((parseInt(this.von) > 0 ? parseInt(this.zuErreichen) / parseInt(this.von) * 100: 0)) + "%";
+      }else{
+        this.perc = parseInt(this.percent) + "%";
       }
     },
-    computed: {
-      gesPoints: function() {
-        let res = 0
-        for (let index in this.gridData){
-          res += parseInt(this.gridData[index]['points'])
-        }
-        return res
-      },
-      gesMaxPoints: function() {
-        let res = 0
-        for (let index in this.gridData){
-          res += parseInt(this.gridData[index]['maxPoints'])
-        }
-        return res
-      },
-      gesPres: function() {
-        let res = 0
-        for (let index in this.gridData){
-          res += parseInt(this.gridData[index]['presentations'])
-        }
-        return res
-      },
-      gesPerc: function() {
-        return this.gesPoints ? Math.round(this.gesPoints/this.gesMaxPoints * 1000)/10 + '%' : 0 + '%'
+  },  
+  computed: {
+    gesPoints: function() {
+      let res = 0;
+      for (let index in this.gridData) {
+        res += parseInt(this.gridData[index]["points"]);
       }
+      return res;
+    },
+    gesMaxPoints: function() {
+      let res = 0;
+      for (let index in this.gridData) {
+        res += parseInt(this.gridData[index]["maxPoints"]);
+      }
+      return res;
+    },
+    gesPres: function() {
+      let res = 0;
+      for (let index in this.gridData) {
+        res += parseInt(this.gridData[index]["presentations"]);
+      }
+      return res;
+    },
+    gesPerc: function() {
+      return this.gesPoints
+        ? Math.round(this.gesPoints / this.gesMaxPoints * 1000) / 10 + "%"
+        : 0 + "%";
     }
   }
+};
 </script>
 
 <style scoped>
@@ -169,7 +250,83 @@
   padding-left: 13px;
   display: flex;
   justify-content: center;
-  
+}
+.grow-enter-active {
+  animation: grow-up 0.9s;
+}
+.fade-leave-active {
+  animation: grow-up 0.5s reverse;
+}
+@keyframes grow-up {
+  0% {
+    transform: scaleY(0);
+  }
+  75% {
+    transform: scaleY(0, 85);
+  }
+  100% {
+    transform: scaleY(1);
+  }
+}
+.settings {
+  background-color: aliceblue;
+  border-radius: 7px;
+  padding-bottom: 5px;
+  padding-top: 5px;
+  font-weight: normal;
+}
+h3 {
+  margin-bottom: 5px;
+  margin-left: 25px;
+}
+
+/*Slider*/
+.switch {
+  position: relative;
+  display: inline-block;
+  width: 30px;
+  height: 14px;
+  margin-top: 5px;
+  margin-left: 5px;
+  margin-right: 5px; 
+}
+.switch input {display:none;}
+.slider {
+  position: absolute;
+  cursor: pointer;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  background-color: #ccc;
+  -webkit-transition: .4s;
+  transition: .4s;
+  border-radius: 34px;
+}
+.slider:before {
+  position: absolute;
+  content: "";
+  height: 10px;
+  width: 10px;
+  left: 2px;
+  bottom: 2px;
+  background-color: white;
+  -webkit-transition: .4s;
+  transition: .4s;
+  border-radius: 50%;
+}
+input:checked + .slider {
+  background-color: #2196F3;
+}
+
+input:focus + .slider {
+  box-shadow: 0 0 1px #2196F3;
+}
+
+input:checked + .slider:before {
+  -webkit-transform: translateX(16px);
+  -ms-transform: translateX(16px);
+  transform: translateX(16px);
 }
 </style>
 
