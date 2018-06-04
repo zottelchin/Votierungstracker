cddb4f1a5447a92d0b0b529dc605340e456372e7
Unknown <zottelchin@gmail.com>
2018-06-04 17:29:59 +0200

    Load CSS from local

--- frontend/src/components/Impressum.vue
+++ frontend/src/components/Impressum.vue
@@ -1,16 +1,20 @@
 <template>
-<div>
+<div class="container">
     <h2>Impressum &amp; Datenschutz</h2>
     <h4>Ersteller</h4>
     <p>Phillipp Engelke<br />
     Albert-Vater-Straße 51<br />
     39108 Magdeburg
     </p>
-    <br />
+    
     <p>
-        Mail: <a href="mailto:kontakt@zottelchin.de">kontakt@zottelchin.de</a><br />
-        Telefon: 0049 151 15985996 
+        Mail: kontakt@zottelchin.de<br />
+        Telefon: +49 151 15985996 
     </p>
+    <h4>Datenschutz</h4>
+    <p>Wir speichern nur Daten, die über die Website eingegeben werden. Wenn ein Benutzer angelegt wird, werden auf unserem Server noch keine Daten gespeichert! 
+        Erst wenn sie die ersten Daten in einem Kurs eintragen wird diese in unserer Datenbank gespeichert.
+        Daten in unserer Datenbank werden mit ihrem Nutzerschlüssel gespeichert. </p>
 </div>
 </template>
 
